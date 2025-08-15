89e3a82d543225f5ea35226aa9bb71ecfa80ba6c
wangheng <Wangheng888>
2025-08-15 11:37:00 +0800

    GKSC

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// 锁定横屏
+			plus.screen.lockOrientation("landscape-primary");
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
